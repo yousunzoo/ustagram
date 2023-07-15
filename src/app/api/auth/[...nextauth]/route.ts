10b0cda96aa74d3ab68bd087a2945ee0b595af0d
yousunzoo <dpfvmffjqm@gmail.com>
2023-07-16 00:49:36 +0900

    Feat: auth api 함수 생성

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+
+const authOptions = {
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_OAUTH_ID || '',
+			clientSecret: process.env.GITHUB_OAUTH_SECRET || '',
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_OAUTH_ID || '',
+			clientSecret: process.env.GOOGLE_OAUTH_SECRET || '',
+		}),
+	],
+};
+
+const handler = NextAuth(authOptions);
+
+export { authOptions, handler as GET, handler as POST };
