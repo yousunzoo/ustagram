1e33e428a268df7496b2c2952bc499186228c155
yousunzoo <dpfvmffjqm@gmail.com>
2023-07-15 23:30:08 +0900

    Feat: Navbar에 들어갈 icon 컴포넌트 생성

---
+++ src/components/ui/icons/HomeActiveIcon.tsx
@@ -0,0 +1,4 @@
+import { GoHomeFill } from 'react-icons/go';
+export default function HomeActiveIcon() {
+	return <GoHomeFill className='w-7 h-7' />;
+}
---
+++ src/components/ui/icons/SearchActiveIcon.tsx
@@ -0,0 +1,4 @@
+import { IoSearch } from 'react-icons/io5';
+export default function SearchActiveIcon() {
+	return <IoSearch className='font-bold w-7 h-7' />;
+}
