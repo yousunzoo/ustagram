d1a11624b28ec158a5b5209ccef020d9dfa6ca39
yousunzoo <dpfvmffjqm@gmail.com>
2023-07-15 23:29:53 +0900

    Feat: Navbar 컴포넌트 생성

---
+++ src/components/common/NavbarLi.tsx
@@ -0,0 +1,26 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React, { ReactNode } from 'react';
+
+interface IItem {
+	href: string;
+	icon_active: ReactNode;
+	icon: ReactNode;
+	title: string;
+}
+function NavbarLi({ item }: { item: IItem }) {
+	const pathname = usePathname();
+	return (
+		<li className='mb-2'>
+			<Link
+				href={item.href}
+				className='p-3 flex items-center gap-4 hover:bg-gray-100 transition ease-in-out duration-200 rounded-md'>
+				{pathname === item.href ? item.icon_active : item.icon}
+				<span className={pathname === item.href ? 'font-bold' : ''}>{item.title}</span>
+			</Link>
+		</li>
+	);
+}
+
+export default NavbarLi;
